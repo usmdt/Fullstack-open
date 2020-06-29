682f2604e22ccbe362cda744fb13f952dc5411f6
Ugnius Smidtas <ugnius@MacBook-Pro.lan>
2020-06-29 22:51:22 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, {useState} from 'react'
 import Note from './Components/Note'
 
 const App = (props) => {
-    const { notes } = props
-  
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState(
+      '... new note'
+    )
+
+    const handleNoteChange = (event) => {
+      console.log(event.target.value)
+      setNewNote(event.target.value)
+    }
+    const addNote = (event) => {
+      event.preventDefault()
+        const noteObject = {
+          id: notes.length +1,
+          content: newNote,
+          date: new Date().toISOString(),
+          important: Math.random() < 0.5
+        }
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+    }
+
     return (
       <div>
         <h1>Notes</h1>
         <ul> 
-              {notes.map(note => <Note key={note.id} content={note.content}/>)}
+              {notes.map(note => 
+              <Note key={note.id} content={note.content}/>
+              )}
         </ul>
+        <form onSubmit={addNote}>
+                <input value={newNote}
+                  onChange={handleNoteChange}>
+                </input>
+                <button type="submit"> save </button>
+        </form>
       </div>
     )
   }
--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 const Note = (props) => {
     return (
